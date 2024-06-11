0d7f421f8c0dae2e5413575e6b810ead1a73d61d
jayalloyd <jayaranilloyd1983@gmail.com>
2024-06-11 06:38:31 +0530

    using session management and cookies

--- src/main/java/SqServlet.java
+++ src/main/java/SqServlet.java
@@ -5,25 +5,36 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class SqServlet
- */
 @WebServlet("/SqServlet")
 public class SqServlet extends HttpServlet {
        
     
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-        int k=Integer.parseInt(request.getParameter("k"));
+//        int k=Integer.parseInt(request.getParameter("k"));
+//        HttpSession session=request.getSession();
+//       int k=(int) session.getAttribute("k");
+		int k=0;
+	Cookie cookie[]=request.getCookies();// using cookies
+	for(Cookie c:cookie) {
+		if(c.getName().equals("k")) {
+			 k=Integer.parseInt(c.getValue());
+		}
+		k=k*k;
+	
+
+       
+	   
            PrintWriter out= response.getWriter();
            out.println("square= "+k);
-	System.out.println("square servlet called");
-	}
+	}}}
 
 	
 
-}
+
--- src/main/java/com/meenuscuisines/AddServlet.java
+++ src/main/java/com/meenuscuisines/AddServlet.java
@@ -7,9 +7,11 @@ import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/AddServlet")
 public class AddServlet extends HttpServlet {
@@ -18,9 +20,15 @@ public class AddServlet extends HttpServlet {
         
         int number1=Integer.parseInt(request.getParameter("num1"));
         int number2=Integer.parseInt(request.getParameter("num2"));
-        int sum=number1+number2;
-		int  k=sum*sum;
-		response.sendRedirect("SqServlet?k="+k);//URL Redirecting
+        int k=number1+number2;
+//		int  k=sum*sum;
+//		HttpSession session=request.getSession();//httpsession management -session management
+//		session.setAttribute("k", k);
+        
+        Cookie cookie=new Cookie("k",k+"");
+        response.addCookie(cookie);
+        
+		response.sendRedirect("SqServlet");//URL Redirecting
 //		request.setAttribute("k", k);
 //		RequestDispatcher rd=request.getRequestDispatcher("/SqServlet");
 //		rd.forward(request, response);
